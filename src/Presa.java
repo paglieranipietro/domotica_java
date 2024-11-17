ef19df1f26d52a777bd6f8b27bcd0e30e44d7e49
P4gl14 <paglieranipietro@gmail.com>
2024-11-17 18:09:35 +0100

    aggiunta attributi e metodi della classe Presa

--- src/Presa.java
+++ src/Presa.java
@@ -1,2 +1,57 @@
 public class Presa {
+    String nome;
+    String stanza;
+    String zona;
+    float x, y;
+    Lampadina lampadina;
+    public Presa(String nome, String stanza, String zona, float x, float y, Lampadina lampadina) {
+        this.nome = nome;
+        this.stanza = stanza;
+        this.zona = zona;
+        this.x = x;
+        this.y = y;
+        this.lampadina = lampadina;
+    }
+    public Presa(String nome, String stanza, String zona, float x, float y) {
+        this.nome = nome;
+        this.stanza = stanza;
+        this.zona = zona;
+        this.x = x;
+        this.y = y;
+        lampadina = null;
+    }
+    public void aggiungiLampadina (Lampadina lampadina) {
+        this.lampadina = lampadina;
+    }
+    public void eliminaLampadina (Lampadina lampadina) {
+        this.lampadina = null;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getStanza() {
+        return stanza;
+    }
+
+    public String getZona() {
+        return zona;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public Lampadina getLampadina() {
+        return lampadina;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
