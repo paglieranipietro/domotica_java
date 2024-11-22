ed494342ff2eb90374f57dec5bc4b7a73dfb6b85
P4gl14 <paglieranipietro@gmail.com>
2024-11-22 19:05:11 +0100

    comandi necessari per aggiunta/eliminazione della lampadina

--- src/Main.java
+++ src/Main.java
@@ -1,50 +1,17 @@
-import controller.UserListener;
-import entity.Lampadina;
 import entity.Presa;
 import entity.Sistema;
 import graphics.Canvas;
-import graphics.Ellipse;
-import graphics.Rectangle;
 import ui.SistemaLayer;
 
-import java.awt.event.KeyListener;
-import java.util.Scanner;
+import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
         Sistema s = new Sistema("s1");
-        s.aggiungiPresa(new Presa("p1","st","zo",0,0));
-        s.aggiungiPresa(new Presa("p2","st","zo",200,300));
+        s.aggiungiPresa(new Presa("p1", "st", "zo", 0, 0));
+        s.aggiungiPresa(new Presa("p2", "st", "zo", 200, 300));
         SistemaLayer.inizializza(s);
-        UserListener userListener = new UserListener(s);
-        Canvas.getInstance().frame.addMouseListener(userListener);
-        boolean continua = true;
-        int scelta;
-        while(continua == true){
-            System.out.println("1. Aggiungi lampadina");
-            System.out.println("2. Rimuovi lampadina");
-            Scanner scanner = new Scanner(System.in);
-            scelta = scanner.nextInt();
-            switch (scelta) {
-                case 1:
-                    System.out.println("Inserisci il nome della presa");
-                    String nomePresa = scanner.next();
-                    System.out.println("Inserisci il nome della lampadina");
-                    String nomeLampadina = scanner.next();
-                    System.out.println("Inserisci la potenza della lampadina");
-                    int potenza = scanner.nextInt();
-                    s.aggiungiLampadinaAPresa(nomePresa, new Lampadina(nomeLampadina, potenza));
-                    break;
-                case 2:
-                    System.out.println("Inserisci il nome della lampadina");
-                    nomePresa = scanner.next();
-                    s.eliminaLampadina(nomePresa);
-                    break;
-                case 3:
-                    continua = false;
-                    break;
-            }
-            SistemaLayer.disegnaPrese();
-        }
+        Canvas.getInstance().frame.addMouseListener(s);
+
     }
 }
