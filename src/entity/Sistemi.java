3ac4aae0b50c17cda69e0f79d3da82d0b1b1f56c
P4gl14 <paglieranipietro@gmail.com>
2024-11-25 18:14:22 +0100

    appunto per aggiornamenti futuri

--- src/entity/Sistemi.java
+++ src/entity/Sistemi.java
@@ -3,22 +3,5 @@
 //import java.util.ArrayList;
 //
 //public class Sistemi {
-//    private ArrayList<Sistema> v;
-//
-//    public Sistemi(){
-//        this.v = new ArrayList<>();
-//    }
-//
-//    public void aggiungiSistema(Sistema sistema){
-//        this.v.add(sistema);
-//    }
-//
-//    public Sistema cercaSistema (String nome){
-//        for (int i = 0; i < this.v.size(); i++) {
-//            if (v.get(i).getNome().equals(nome)){
-//                return v.get(i);
-//            }
-//        }
-//        return null;
-//    }
+//    gestione di più di un sistema ancora da implementare
 //}
